177a3a0644b64847f246359368b2a014408851e0
Johnkai Cortez <73104201+Johnkai2196@users.noreply.github.com>
2023-04-13 10:24:44 +0300

    20/27

--- src/api/resolvers/catResolver.ts
+++ src/api/resolvers/catResolver.ts
@@ -9,3 +9,66 @@ import {Types} from 'mongoose';
 // TODO: create resolvers based on cat.graphql
 // note: when updating or deleting a cat, you need to check if the user is the owner of the cat
 // note2: when updating or deleting a cat as admin, you need to check if the user is an admin by checking the role from the user object
+export default {
+  // 1. Queries
+  Query: {
+    // 1.1. Get all cats
+    cats: async () => {
+      return await catModel.find();
+    },
+    // 1.2. Get a cat by id
+    catById: async (_parent: unknown, args: Cat) => {
+      return await catModel.findById(args.id);
+    },
+    // 1.3. Get all cats by owner
+    catsByOwner: async (_parent: unknown, args: UserIdWithToken) => {
+      return await catModel.find({owner: args.id});
+    },
+    // 1.4. Get all cats by location
+    catsByArea: async (_parent: unknown, args: locationInput) => {
+      const bounds = rectangleBounds(args.topRight, args.bottomLeft);
+      return await catModel.find({
+        location: {
+          $geoWithin: {
+            $geometry: bounds,
+          },
+        },
+      });
+    },
+  },
+  // 2. Mutations
+  Mutation: {
+    // 2.1. Create a cat
+    createCat: async (_parent: unknown, args: Cat, user: UserIdWithToken) => {
+      console.log(user);
+
+      if (!user.token) {
+        throw new GraphQLError('Not authorized', {
+          extensions: {code: 'NOT_AUTHORIZED'},
+        });
+      }
+      args.owner = user.id as unknown as Types.ObjectId;
+      const cat = new catModel(args);
+      return await cat.save();
+    },
+    // 2.2. Update a cat
+    updateCat: async (_parent: unknown, args: Cat, user: UserIdWithToken) => {
+      console.log(user);
+      if (!user.token) {
+        throw new GraphQLError('Not authorized', {
+          extensions: {code: 'NOT_AUTHORIZED'},
+        });
+      }
+      return await catModel.findByIdAndUpdate(args.id, args, {new: true});
+    },
+    // 2.3. Delete a cat
+    deleteCat: async (_parent: unknown, args: Cat, user: UserIdWithToken) => {
+      if (!user.token) {
+        throw new GraphQLError('Not authorized', {
+          extensions: {code: 'NOT_AUTHORIZED'},
+        });
+      }
+      return await catModel.findByIdAndDelete(args.id);
+    },
+  },
+};
--- src/api/resolvers/userResolver.ts
+++ src/api/resolvers/userResolver.ts
@@ -5,3 +5,179 @@ import {User, UserIdWithToken} from '../../interfaces/User';
 // TODO: create resolvers based on user.graphql
 // note: when updating or deleting a user don't send id to the auth server, it will get it from the token
 // note2: when updating or deleting a user as admin, you need to check if the user is an admin by checking the role from the user object
+export default {
+  Cat: {
+    owner: async (parent: Cat) => {
+      const response = await fetch(
+        `${process.env.AUTH_URL}/users/${parent.owner}`
+      );
+      if (!response.ok) {
+        throw new GraphQLError(response.statusText, {
+          extensions: {
+            code: 'NOT_FOUND',
+          },
+        });
+      }
+      const user = (await response.json()) as User;
+      return user;
+    },
+  },
+  // 1. Queries
+  Query: {
+    // 1.1. Get all users
+    users: async () => {
+      const response = await fetch(`${process.env.AUTH_URL}/users`);
+      if (!response.ok) {
+        throw new GraphQLError(response.statusText, {
+          extensions: {
+            code: 'NOT_FOUND',
+          },
+        });
+      }
+      const users = (await response.json()) as User[];
+      return users;
+    },
+    // 1.2. Get a user by id
+    userById: async (_parent: unknown, args: {id: string}) => {
+      const response = await fetch(`${process.env.AUTH_URL}/users/${args.id}`);
+      if (!response.ok) {
+        throw new GraphQLError(response.statusText, {
+          extensions: {
+            code: 'NOT_FOUND',
+          },
+        });
+      }
+      const user = (await response.json()) as User;
+      return user;
+    },
+    // 1.3. check token
+    checkToken: async (
+      _parent: unknown,
+      _args: unknown,
+      user: UserIdWithToken
+    ) => {
+      const response = await fetch(`${process.env.AUTH_URL}/users/token`, {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      });
+      if (!response.ok) {
+        throw new GraphQLError(response.statusText, {
+          extensions: {
+            code: 'NOT_FOUND',
+          },
+        });
+      }
+      const userFromAuth = await response.json();
+      return userFromAuth;
+    },
+  },
+  // 2. Mutations
+  Mutation: {
+    // 2.1. Register a user
+    register: async (_parent: unknown, args: {user: User}) => {
+      console.log(args.user);
+
+      const response = await fetch(`${process.env.AUTH_URL}/users`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(args.user),
+      });
+      if (!response.ok) {
+        throw new GraphQLError(response.statusText, {
+          extensions: {
+            code: 'NOT_FOUND',
+          },
+        });
+      }
+      const user = (await response.json()) as LoginMessageResponse;
+      return user;
+    },
+    // 2.2 Login a user
+    login: async (
+      _parent: unknown,
+      args: {credentials: {username: string; password: string}}
+    ) => {
+      const response = await fetch(`${process.env.AUTH_URL}/auth/login`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(args.credentials),
+      });
+      if (!response.ok) {
+        throw new GraphQLError(response.statusText, {
+          extensions: {
+            code: 'NOT_FOUND',
+          },
+        });
+      }
+      const user = (await response.json()) as LoginMessageResponse;
+      return user;
+    },
+    // 2.3. Update a user
+    updateUser: async (
+      _parent: unknown,
+      args: {user: User},
+      user: UserIdWithToken
+    ) => {
+      if (!user.token) {
+        throw new GraphQLError('Unauthorized', {
+          extensions: {
+            code: 'UNAUTHORIZED',
+          },
+        });
+      }
+      const response = await fetch(`${process.env.AUTH_URL}/users/`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${user.token}`,
+        },
+        body: JSON.stringify(args.user),
+      });
+      if (!response.ok) {
+        throw new GraphQLError(response.statusText, {
+          extensions: {
+            code: 'NOT_FOUND',
+          },
+        });
+      }
+      const userUpdated = (await response.json()) as LoginMessageResponse;
+      return userUpdated;
+    },
+    // 2.4. Delete a user
+    deleteUser: async (
+      _parent: unknown,
+      args: unknown,
+      user: UserIdWithToken
+    ) => {
+      if (!user.token) {
+        throw new GraphQLError('Unauthorized', {
+          extensions: {
+            code: 'UNAUTHORIZED',
+          },
+        });
+      }
+      const response = await fetch(`${process.env.AUTH_URL}/users`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${user.token}`,
+        },
+        body: JSON.stringify(args),
+      });
+      if (!response.ok) {
+        throw new GraphQLError(response.statusText, {
+          extensions: {
+            code: 'NOT_FOUND',
+          },
+        });
+      }
+      const userDeleted = (await response.json()) as LoginMessageResponse;
+      return userDeleted;
+    },
+  },
+};
